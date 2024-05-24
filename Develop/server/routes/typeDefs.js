e8872d9d425dd6bd46c751be647780497c6d7248
IsItIzzet <ohlhjames@gmail.com>
2024-05-24 16:14:44 -0500

    Added Type Defs so that graphQl can use them

---
+++ Develop/server/routes/typeDefs.js
@@ -0,0 +1,36 @@
+const typeDefs = `
+  type User {
+    _id: ID
+    name: String
+    email: String
+    password: String
+    skills: [String]!
+  }
+
+  type Auth {
+    token: ID!
+    profile: Profile
+  }
+
+  type Book {
+    authors:[String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Query {
+    user(userId: ID!): Profile
+  }
+
+  type Mutation {
+    addUser(name: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook: Book
+    deleteBook: Book
+  }
+`;
+
+module.exports = typeDefs;
