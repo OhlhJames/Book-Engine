5f45799605ff2e1ac06aa95eff199cb20eaefd60
IsItIzzet <ohlhjames@gmail.com>
2024-05-24 14:09:09 -0500

    Added graphql code for auth tokens as well as integrating graphql into the program

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import {ApolloClient, InMemoryCache, ApolloProvider, createHttpLink,} from '@apollo/client';
 import { Outlet } from 'react-router-dom';
-
+import { setContext } from '@apollo/client/link/context'
 import Navbar from './components/Navbar';
 
 const httpLink = createHttpLink({
